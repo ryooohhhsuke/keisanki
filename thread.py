9ccd2c82ae991667aa8d6177b586ff7a03acb8e0
ryooohhhsuke <52556931+ryooohhhsuke@users.noreply.github.com>
2020-07-03 14:21:55 +0900

    updat
    最後のif文以下を修正しました。

--- thread.py
+++ thread.py
@@ -22,7 +22,7 @@ def my_function3():
         print("GHI")
 
 if __name__ == "__main__":
-  
+  semaphore=Semaphore(1)
   t1 = Thread(target = my_function1)
   t2 = Thread(target = my_function2)
   t3 = Thread(target = my_function3)
