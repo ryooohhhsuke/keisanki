9367e2a4b6fdecc6e8d0617f79eadd239a0aa2c5
ryooohhhsuke <52556931+ryooohhhsuke@users.noreply.github.com>
2020-07-03 13:57:06 +0900

    updat.py
    semaphoreとsleepを組み込みました。

--- thread.py
+++ thread.py
@@ -1,24 +1,32 @@
-from threading import Thread
+from threading import Thread,semaphore
+import time
 
 def my_function1():
   for i in range(10000):
-    print("123")
-    print("456")
-    print("789")
+    with semaphore:
+      print("123")
+      print("456")
+      print("789")
+      time.sleep(1)
 
 def my_function2():
   for i in range(10000):
-    print("abc")
-    print("def")
-    print("ghi")
+    with semaphtore:
+      print("abc")
+      print("def")
+      print("ghi")
+      time.sleep(1)
 
 def my_function3():
   for i in range(10000):
-    print("ABC")
-    print("DEF")
-    print("GHI")
+    with semaphore:
+      print("ABC")
+      print("DEF")
+      print("GHI")
+      time.sleep(1)
 
 if __name__ == "__main__":
+  semaphore=Semaphore(1)
   t1 = Thread(target = my_function1)
   t2 = Thread(target = my_function2)
   t3 = Thread(target = my_function3)
